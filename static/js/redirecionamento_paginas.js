32c3ff332e670ff528ec5f75f4624ea640d9872c
aliinescholl <Aline.scholl13@gmail.com>
2022-11-06 10:16:06 -0300

    dropdown troca entre as paginas

---
+++ static/js/redirecionamento_paginas.js
@@ -0,0 +1,6 @@
+function IrJurosCompostos(){
+    window.location.href = "https://aliinescholl.github.io/templates/calc_juros_compostos.html";
+}
+function IrJurosSimples(){
+    window.location.href = "https://aliinescholl.github.io/templates/calc_juros_simples.html";
+}
