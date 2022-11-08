58a5cd84ae8de1e977ccd649129c1dcfc69f97c7
Aline Scholl Santos <76217058+aliinescholl@users.noreply.github.com>
2022-11-08 11:30:39 -0300

    assign funfa na index

--- static/js/redirecionamento_paginas.js
+++ static/js/redirecionamento_paginas.js
@@ -2,5 +2,11 @@ function IrJurosCompostos(){
     window.location.assign("/templates/calc_juros_compostos.html");
 }
 function IrJurosSimples(){
-    window.location.assign("templates/calc_juros_simples.html");
+    window.location.assign("/templates/calc_juros_simples.html");
+}
+function IrJurosCompostosCalculadora(){
+    window.location.assign("calc_juros_compostos.html");
+}
+function IrJurosSimplesCalculadora(){
+    window.location.assign("calc_juros_simples.html");
 }
